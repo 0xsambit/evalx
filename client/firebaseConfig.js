49596e132e2c1c21b29de4bd8e231694dc827852
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-09-29 23:36:38 +0530

    Add firebase configuration file

---
+++ client/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDOd9UJ5mDEx4QbBobsFM7o3lKuigj2auY",
+	authDomain: "codemate-01.firebaseapp.com",
+	projectId: "codemate-01",
+	storageBucket: "codemate-01.appspot.com",
+	messagingSenderId: "776826410761",
+	appId: "1:776826410761:web:93518bda9db613f82165d8",
+	measurementId: "G-H2L9MVJ6TW",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
