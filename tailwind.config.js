7ca42cb9f9c4a74e12d9f3baa1153265c5d8ac27
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-11-20 00:18:11 +0530

    Update Tailwind CSS configuration to enable dark mode and enhance color palette with new shades

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: "#2C2B2A",
-          '100': "#F8F8F8",
-          '200': "#E5E5E5",
-        },
-        secondary: {
-          DEFAULT: "#B2B1B1",
-          '100': "#F6F6F6",
-        },
-        astral: "#346DA3",
-        glacier: "#81A7C2"
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: {
+  				'100': '#5a5a5a',
+  				'200': '#4b4b4b',
+  				DEFAULT: '#212121'
+  			},
+  			secondary: {
+  				'100': '#a5a5a5',
+  				'200': '#cdcdcd',
+  				DEFAULT: '#969696'
+  			},
+  			astral: '#346DA3',
+  			glacier: '#81A7C2'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
