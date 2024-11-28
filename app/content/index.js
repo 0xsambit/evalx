ba465924e3ad927369d2e4d6639529d592e7cdab
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-11-28 13:56:45 +0530

    Add logo module and FAQ section to enhance content structure

--- app/content/index.js
+++ app/content/index.js
@@ -1,5 +1,5 @@
 import { LaptopMinimalCheck, LayoutDashboardIcon, User } from "lucide-react";
-
+import logo from "@/app/content/logo";
 export const CardContent = [
     {
         id: 1,
@@ -41,3 +41,32 @@ export const Testimonials = [
         desc: "I have been using EvalX for a while now and I must say it is the best platform to conduct interviews. The platform is very user-friendly and easy to use. I would recommend this platform to everyone.",
     },
 ];
+
+export const FAQ = [
+    {
+        id: 1,
+        tagline: "Is EvalX suitable for remote interviews?",
+        desc: "Absolutely! EvalX is built with remote interviews in mind, offering live coding, video calls, and real-time collaboration tools.",
+    },
+    {
+        id: 2,
+        tagline: "What integrations does EvalX support?",
+        desc: "EvalX supports popular programming languages like Python, JavaScript, Java, C++, and more in its live coding environment.",
+    },
+    {
+        id: 3,
+        tagline: "How does EvalX help me evaluate candidates?",
+        desc: "EvalX uses industry-standard encryption and secure authentication methods to protect user data and interview content.",
+    },
+    {
+        id: 4,
+        tagline: "Can I track candidate performance on EvalX?",
+        desc: "Yes, EvalX provides detailed analytics on candidate performance, allowing you to track their progress and identify areas for improvement. You can use this data to make data-driven hiring decisions.",
+    },
+    {
+        id: 5,
+        tagline: "Is EvalX easy to use?",
+        desc: "Yes, EvalX is designed to be user-friendly and intuitive. You can easily set up interviews, invite candidates, and evaluate their performance with just a few clicks. The platform also offers real-time collaboration tools to make the interview process seamless.",
+    },
+];
+export { logo };
---
+++ app/content/logo.js
@@ -0,0 +1,5 @@
+import github from "@/app/assets/github.png";
+import google from "@/app/assets/google.png";
+import slack from "@/app/assets/Slack-logo-White.png";
+const logo = { github, google, slack };
+export default logo;
