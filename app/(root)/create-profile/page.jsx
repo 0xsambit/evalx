e39f029cff2ca6bf211145cf5d8a98805d43db3f
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-12-01 13:38:03 +0530

    Candidate and Reecruiter profile page seperated

--- app/(root)/create-profile/page.jsx
+++ app/(root)/create-profile/page.jsx
@@ -7,8 +7,9 @@ import { skills } from "@/app/content";
 import { useState, useEffect } from "react";
 import { Button } from "@/app/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import Footer from "@/app/components/Footer";
 const Profile = () => {
-    const [role, setRole] = useState("");
+    const [role, setRole] = useState("Candidate");
     const [email, setEmail] = useState("");
     const [image, setImage] = useState(null);
 
@@ -129,96 +130,148 @@ const Profile = () => {
                     </div>
                 </div>
             </div>
-            <div className='section'>
-                <h1 className='text-3xl font-semibold text-center my-3'>
-                    Profile Details
-                </h1>
-                <form className='flex flex-col justify-center items-start px-10'>
-                    <h1 className='text-xl font-medium'>Skills / Expertise</h1>
-                    {skills.map((skill) => (
-                        <div
-                            className='flex  justify-center items-center gap-5 h-10'
-                            key={skill.id}>
-                            <Checkbox label={skill.code} className='bg-white' />
-                            <label htmlFor='skills'>{skill.name}</label>
-                        </div>
-                    ))}
-                    <input
-                        type='text'
-                        placeholder='Others ..'
-                        className='bg-inherit border border-white outline-none px-5 py-2 rounded-xl text-gray-300 mt-5'
-                    />
-                    <div className='flex justify-center items-center gap-5'>
-                        <h1 className='text-xl font-medium mt-6 mb-4'>
-                            Experience
+            {role == "Candidate" ? (
+                <>
+                    <div className='section'>
+                        <h1 className='text-3xl font-semibold text-center my-3'>
+                            Profile Details
                         </h1>
-                        <input type='radio' />
-                        <label htmlFor='experience'>Fresher</label>
-                        <input type='radio' />
-                        <label htmlFor='experience'>1-2 years</label>
-                        <input type='radio' />
-                        <label htmlFor='experience'>3-5 years</label>
-                        <input type='radio' />
-                        <label htmlFor='experience'>5+ years</label>
+                        <form className='flex flex-col justify-center items-start px-10'>
+                            <h1 className='text-xl font-medium'>
+                                Skills / Expertise
+                            </h1>
+                            {skills.map((skill) => (
+                                <div
+                                    className='flex  justify-center items-center gap-5 h-10'
+                                    key={skill.id}>
+                                    <Checkbox
+                                        label={skill.code}
+                                        className='bg-white'
+                                    />
+                                    <label htmlFor='skills'>{skill.name}</label>
+                                </div>
+                            ))}
+                            <input
+                                type='text'
+                                placeholder='Others ..'
+                                className='bg-inherit border border-white outline-none px-5 py-2 rounded-xl text-gray-300 mt-5'
+                            />
+                            <div className='flex justify-center items-center gap-5'>
+                                <h1 className='text-xl font-medium mt-6 mb-4'>
+                                    Experience
+                                </h1>
+                                <input type='radio' />
+                                <label htmlFor='experience'>Fresher</label>
+                                <input type='radio' />
+                                <label htmlFor='experience'>1-2 years</label>
+                                <input type='radio' />
+                                <label htmlFor='experience'>3-5 years</label>
+                                <input type='radio' />
+                                <label htmlFor='experience'>5+ years</label>
+                            </div>
+                            <div className='flex justify-start items-center gap-10 w-full my-5'>
+                                <label
+                                    htmlFor='linkedin'
+                                    className='font-medium text-lg w-1/4'>
+                                    LinkedIn Profile URL
+                                </label>
+                                <input
+                                    type='url'
+                                    id='linkedin'
+                                    name='linkedin'
+                                    placeholder='Enter your LinkedIn profile URL'
+                                    className='bg-inherit border border-white outline-none px-5 py-2 rounded-xl text-gray-300 w-1/2'
+                                />
+                            </div>
+                            <div className='flex justify-start items-center gap-10 w-full my-5'>
+                                <label
+                                    htmlFor='github'
+                                    className='font-medium text-lg w-1/4'>
+                                    Github Profile URL
+                                </label>
+                                <input
+                                    type='url'
+                                    id='github'
+                                    name='github'
+                                    placeholder='Enter your Github profile URL'
+                                    className='bg-inherit border border-white outline-none px-5 py-2 rounded-xl text-gray-300 w-1/2'
+                                />
+                            </div>
+                            <div className='flex justify-start items-center gap-10 w-full my-5'>
+                                <label
+                                    htmlFor='portfolio'
+                                    className='font-medium text-lg w-1/4'>
+                                    Portfolio URL
+                                </label>
+                                <input
+                                    type='url'
+                                    id='portfolio'
+                                    name='portfolio'
+                                    placeholder='Enter your Portfolio URL'
+                                    className='bg-inherit border border-white outline-none px-5 py-2 rounded-xl text-gray-300 w-1/2'
+                                />
+                            </div>
+                        </form>
                     </div>
-                    <div className='flex justify-start items-center gap-10 w-full my-5'>
-                        <label
-                            htmlFor='linkedin'
-                            className='font-medium text-lg w-1/4'>
-                            LinkedIn Profile URL
-                        </label>
-                        <input
-                            type='url'
-                            id='linkedin'
-                            name='linkedin'
-                            placeholder='Enter your LinkedIn profile URL'
-                            className='bg-inherit border border-white outline-none px-5 py-2 rounded-xl text-gray-300 w-1/2'
+                    <div className='section'>
+                        <h1 className='font-semibold text-3xl text-center mb-5'>
+                            Additional Information
+                        </h1>
+                        <p className='mx-10 font-medium text-xl'>
+                            {" "}
+                            Describe Yourself
+                        </p>
+                        <Textarea
+                            placeholder='Describe abit about yourself'
+                            className='w-2/3 m-auto'
                         />
                     </div>
-                    <div className='flex justify-start items-center gap-10 w-full my-5'>
-                        <label
-                            htmlFor='github'
-                            className='font-medium text-lg w-1/4'>
-                            Github Profile URL
-                        </label>
-                        <input
-                            type='url'
-                            id='github'
-                            name='github'
-                            placeholder='Enter your Github profile URL'
-                            className='bg-inherit border border-white outline-none px-5 py-2 rounded-xl text-gray-300 w-1/2'
-                        />
+                    <div className='flex justify-center items-center m-auto w-full my-10'>
+                        <Button className='bg-white text-black font-medium text-lg rounded-xl w-2/3 hover:bg-black hover:text-white duration-200 ease-linear'>
+                            Submit your details
+                        </Button>
                     </div>
-                    <div className='flex justify-start items-center gap-10 w-full my-5'>
-                        <label
-                            htmlFor='portfolio'
-                            className='font-medium text-lg w-1/4'>
-                            Portfolio URL
-                        </label>
-                        <input
-                            type='url'
-                            id='portfolio'
-                            name='portfolio'
-                            placeholder='Enter your Portfolio URL'
-                            className='bg-inherit border border-white outline-none px-5 py-2 rounded-xl text-gray-300 w-1/2'
-                        />
+                </>
+            ) : (
+                <>
+                    <div className='section'>
+                        <h1 className='text-3xl font-semibold text-center my-3'>
+                            Company Details
+                        </h1>
+                        <form className='flex flex-col justify-center items-start px-10'>
+                            <div className='flex justify-start items-center gap-10 w-full my-5'>
+                                <label
+                                    htmlFor='portfolio'
+                                    className='font-medium text-lg w-1/4'>
+                                    Company Name
+                                </label>
+                                <input
+                                    type='url'
+                                    id='company'
+                                    name='company'
+                                    placeholder='Your Company Name'
+                                    className='bg-inherit border border-white outline-none px-5 py-2 rounded-xl text-gray-300 w-1/2'
+                                />
+                            </div>
+                            <div className='flex justify-start items-center gap-10 w-full my-5'>
+                                <label
+                                    htmlFor='portfolio'
+                                    className='font-medium text-lg w-1/4'>
+                                    Job Role / Position
+                                </label>
+                                <input
+                                    type='url'
+                                    id='job'
+                                    name='job'
+                                    placeholder='Specify the Job Role requirement for the candidate'
+                                    className='bg-inherit border border-white outline-none px-5 py-2 rounded-xl text-gray-300 w-1/2'
+                                />
+                            </div>
+                        </form>
                     </div>
-                </form>
-            </div>
-            <div className='section'>
-                <h1 className='font-semibold text-3xl text-center mb-5'>
-                    Additional Information
-                </h1>
-                <Textarea
-                    placeholder='Describe abit about yourself'
-                    className='w-2/3 m-auto'
-                />
-            </div>
-            <div className='flex justify-center items-center m-auto w-full'>
-                <Button className='bg-white text-black font-medium text-lg rounded-xl w-3/4 hover:bg-black hover:text-white duration-200 ease-linear'>
-                    Submit
-                </Button>
-            </div>
+                </>
+            )}
+            <Footer />
         </section>
     );
 };
